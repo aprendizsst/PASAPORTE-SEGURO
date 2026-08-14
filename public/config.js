d725a414fec3cad0a1b6902a995e844f97d15549
aprendizsst <aprendiz.sst@jer.com.co>
2026-08-14 16:13:14 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 // Pegue entre comillas la URL /exec de su implementación de Google Apps Script.
 // Mientras esté vacío, la aplicación funciona en modo demostración.
 window.PASSPORT_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzc4_V7WdPV9Fj83eDelbRuFfn_V28CzHuCIcT9ugiopAzL8PjoLlhegr3Q5-tNc5pc/exec",
 };
