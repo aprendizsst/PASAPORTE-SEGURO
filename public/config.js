be02cd4a3e08ee9336bfd3ee740470043e685c93
aprendizsst <aprendiz.sst@jer.com.co>
2026-09-01 16:30:39 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 // Si queda vacío, la aplicación bloqueará el acceso y mostrará un aviso. Esta URL
 // es administrativa: los participantes no pueden cambiarla desde la aplicación.
 window.PASSPORT_CONFIG = Object.freeze({
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzc4_V7WdPV9Fj83eDelbRuFfn_V28CzHuCIcT9ugiopAzL8PjoLlhegr3Q5-tNc5pc/exec",
   // Puede desactivar una mejora visual sin borrar código ni modificar Google Sheets.
   features: Object.freeze({
     dynamicCover: true,
